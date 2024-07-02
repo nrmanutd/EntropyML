648a3da1210ac3f033a9a21e87c41c91e6021bd1
nrmanutd <nrmanutd@gmail.com>
2024-07-02 18:18:25 +0300

    added entropy calculation

--- calcEntropy.py
+++ calcEntropy.py
@@ -50,12 +50,112 @@ def calcAndVisualize(dataSet, cl):
     plt.show()
 
 def calcConditionalEntropy(dataSet, buckets, classIdx):
-    return 1
-def calcConditionalMultiVarianceEntropy(dataSet, buckets, classIdx):
-    return 1
+    nObjects = dataSet.shape[0]
+    nClasses = len(classIdx)
+    nFeatures = dataSet.shape[1]
+
+    conditionalEntropy = 0.0
+
+    for iFeature in np.arange(nFeatures):
+        for jClass in np.arange(nClasses):
+            idx = np.digitize(dataSet[classIdx[jClass], iFeature], buckets[:, iFeature])
+            v, c = np.unique(idx, return_counts=True)
+            val = c / sum(c)
+            conditionalEntropy += sum(c) / nObjects * entropy(c / sum(c))
+
+    return conditionalEntropy
+def calcConditionalMultiVarianceEntropy(dataSet, objectBins, classIdx):
+    nObjects = dataSet.shape[0]
+    nClasses = len(classIdx)
+
+    conditionalMultiEntropy = np.zeros(nClasses, dtype=float)
+
+    for iClass in np.arange(nClasses):
+        mve = calcMultiVarianceEntropy(dataSet[classIdx[iClass], :], objectBins)
+        conditionalMultiEntropy[iClass] = len(classIdx[iClass])/nObjects * mve
+
+    return sum(conditionalMultiEntropy)
 
 def calcSimpleEntropy(variable, buckets):
     return 1
 
-def calcMultiVarianceEntropy(dataSet, buckets, classIdx):
-    return 1
+def calcMultiVarianceEntropy(dataSet, objectBins):
+    nObjects = dataSet.shape[0]
+    nFeatures = dataSet.shape[1]
+
+    objectBinsIndexes = np.zeros((nObjects, nFeatures))
+
+    for iFeature in np.arange(nFeatures):
+        objectBinsIndexes[:, iFeature] = np.digitize(dataSet[:, iFeature], objectBins[:, iFeature])
+
+    objectBinsIndexes = 10 ** objectBinsIndexes
+    bucketIdx = np.zeros(nObjects)
+
+    for iObject in np.arange(nObjects):
+        bucketIdx[iObject] = sum(objectBinsIndexes[iObject, :])
+
+    eBins, cBins = np.unique(bucketIdx, return_counts=True)
+
+    return entropy(cBins/sum(cBins))
+
+def calculateAndVisualizeSeveralEntropies(dataSet, target):
+    nFeatures = dataSet.shape[1]
+    nObjects = dataSet.shape[0]
+
+    entries, counts = np.unique(target, return_counts=True)
+
+    cl = []
+    for iClass in np.arange(0, len(entries)):
+        cl.append(np.where(target == entries[iClass])[0])
+
+    # totalBins = math.ceil(nObjects ** (1 / nFeatures))
+    # totalBins = 10
+
+    bins = np.arange(math.ceil(nObjects ** (1 / nFeatures)), 100)
+
+    efficiency = np.zeros(len(bins), dtype=float)
+    simple = np.zeros(len(bins), dtype=float)
+    conditional = np.zeros(len(bins), dtype=float)
+    multiEntropy = np.zeros(len(bins), dtype=float)
+    multiConditionalEntropy = np.zeros(len(bins), dtype=float)
+
+    for iBins in np.arange(len(bins)):
+        totalBins = bins[iBins]
+        objectBins = np.zeros((totalBins + 1, nFeatures))
+        simpleEntropy = np.zeros(nFeatures + 1, dtype=float)
+
+        for i in np.arange(nFeatures):
+            h = np.histogram(dataSet[:, i], bins=totalBins, density=True)
+            objectBins[:, i] = h[1]
+            simpleEntropy[i] = entropy(h[0])
+
+        simpleEntropy[nFeatures] = entropy(counts / nObjects)
+        # print('Total bins: ', totalBins)
+        # print('Simple entropy: ', simpleEntropy)
+        # print('Conditional entropy: ', conditionalEntropy)
+
+        # print('Simple number of letters: ', math.e ** simpleEntropy)
+        # print('Conditional number of letters: ', math.e ** conditionalEntropy)
+
+        simple[iBins] = sum(simpleEntropy)
+        conditional[iBins] = calcConditionalEntropy(dataSet, objectBins, cl)
+        multiEntropy[iBins] = calcMultiVarianceEntropy(dataSet, objectBins)
+        multiConditionalEntropy[iBins] = calcConditionalMultiVarianceEntropy(dataSet, objectBins, cl)
+
+        # efficiency[iBins] = math.e ** sum(simpleEntropy) / math.e ** sum(conditionalEntropy)
+        efficiency[iBins] = conditional[iBins] - multiConditionalEntropy[iBins]
+
+        # print('Simple: {0: 5.1f}, Conditional: {1: 5.1f}, Efficiency: {2: 5.1f}'.format(simpleTotalWords, conditionalTotalWords, simpleTotalWords/conditionalTotalWords))
+
+    fig, ax = plt.subplots(5, 1, sharex=True, tight_layout=True)
+
+    ax[0].plot(bins, efficiency)
+    ax[1].plot(bins, simple)
+    ax[2].plot(bins, conditional)
+    ax[3].plot(bins, multiEntropy)
+    ax[4].plot(bins, multiConditionalEntropy)
+
+    # ax[3].plot(simple, conditional)
+
+    plt.show()
+    return
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import math
 
 import pandas as pd
 
-from calcEntropy import calcAndVisualize, calcSimpleEntropy, calcConditionalEntropy
+from calcEntropy import calcAndVisualize, calcSimpleEntropy, calcConditionalEntropy, calcMultiVarianceEntropy, \
+    calcConditionalMultiVarianceEntropy, calculateAndVisualizeSeveralEntropies
 from ucimlrepo import fetch_ucirepo
 from scipy.stats import norm, entropy
 import plotly.express as px
@@ -67,95 +68,15 @@ def showCircles():
     #calcAndVisualize(dataSet, cl, 10)
 #showCircles()
 
-def test():
-    np.random.seed(19680801)
-
-    N = 100
-    r0 = 0.6
-    x = 0.9 * np.random.rand(N)
-    y = 0.9 * np.random.rand(N)
-    area = (20 * np.random.rand(N)) ** 2  # 0 to 10 point radii
-    c = np.sqrt(area)
-    r = np.sqrt(x ** 2 + y ** 2)
-    area1 = np.ma.masked_where(r < r0, area)
-    area2 = np.ma.masked_where(r >= r0, area)
-    plt.scatter(x, y, s=area1, marker='^', c=c)
-    plt.scatter(x, y, s=area2, marker='o', c=c)
-    # Show the boundary between the regions:
-    theta = np.arange(0, np.pi / 2, 0.01)
-    plt.plot(r0 * np.cos(theta), r0 * np.sin(theta))
-
-    plt.show()
-#test()
-
 def calculateMultiEntropyIris():
     # fetch dataset
     iris = fetch_ucirepo(id=53)
 
     # data (as pandas dataframes)
     y = iris.data.targets
-    entries, counts = np.unique(y, return_counts=True)
-    nClasses = len(entries)
-
     dataSet = np.array(iris.data.features)
-    nFeatures = dataSet.shape[1]
-    nObjects = dataSet.shape[0]
 
-    idx1 = np.arange(50)
-    idx2 = np.arange(50, 100)
-    idx3 = np.arange(100, 150)
-
-    cl = np.array([idx1, idx2, idx3])
-
-    #totalBins = math.ceil(nObjects ** (1 / nFeatures))
-    # totalBins = 10
-
-    bins = np.arange(math.ceil(nObjects ** (1 / nFeatures)), 100)
-    efficiency = np.zeros(len(bins), dtype=float)
-    simple = np.zeros(len(bins), dtype=float)
-    conditional = np.zeros(len(bins), dtype=float)
-
-    for iBins in np.arange(len(bins)):
-        totalBins = bins[iBins]
-
-        simpleEntropy = np.zeros(nFeatures + 1, dtype=float)
-        conditionalEntropy = np.zeros(nFeatures, dtype=float)
-
-        for i in np.arange(nFeatures):
-            h = np.histogram(dataSet[:, i], bins=totalBins, density=True)
-            ent = entropy(h[0])
-            simpleEntropy[i] = ent
-
-            for jClass in np.arange(nClasses):
-                hConditional = np.histogram(dataSet[cl[jClass], i], bins=h[1], density=True)
-                conditionalEntropy[i] += len(cl[jClass]) / nObjects * entropy(hConditional[0])
-
-        simpleEntropy[nFeatures] = entropy(counts/nObjects)
-
-        #print('Total bins: ', totalBins)
-        #print('Simple entropy: ', simpleEntropy)
-        #print('Conditional entropy: ', conditionalEntropy)
-
-        #print('Simple number of letters: ', math.e ** simpleEntropy)
-        #print('Conditional number of letters: ', math.e ** conditionalEntropy)
-
-        simpleTotalWords = math.e ** sum(simpleEntropy)
-        conditionalTotalWords = math.e ** sum(conditionalEntropy)
-
-        efficiency[iBins] = simpleTotalWords/conditionalTotalWords
-        simple[iBins] = simpleTotalWords
-        conditional[iBins] = conditionalTotalWords
-
-        #print('Simple: {0: 5.1f}, Conditional: {1: 5.1f}, Efficiency: {2: 5.1f}'.format(simpleTotalWords, conditionalTotalWords, simpleTotalWords/conditionalTotalWords))
-
-    fig, ax = plt.subplots(1, 1, tight_layout=True)
-
-    ax.plot(bins, efficiency)
-    #ax[1].plot(bins, simple)
-    #ax[2].plot(bins, conditional)
-    #ax[3].plot(simple, conditional)
-
-    plt.show()
+    calculateAndVisualizeSeveralEntropies(dataSet, y)
 
     return
 
