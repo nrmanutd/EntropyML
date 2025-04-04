2af6e32ebc397e7f3535fda0971c0943e70e0c6c
nrmanutd <nrmanutd@gmail.com>
2025-04-04 23:10:10 +0300

    updated algorithm, added permutation logic calculation and visualization

--- CodeResearch/StatisticsAnalyzer/dataLoader.py
+++ CodeResearch/StatisticsAnalyzer/dataLoader.py
@@ -1,17 +1,14 @@
 from CodeResearch.Visualization.saveDataForVisualization import deserialize_labeles_list_of_arrays
 
 def getFileName(taskName, type):
-    if taskName == 'mnist' and type == 'KS':
-        return '..\\Data\\mnist\\27.03_logs\\{:}_mnist_1000_9_8.txt'.format(type)
+
+    amount = 1000 if type == 'KS' else 100
 
     if taskName == 'mnist':
-        return '..\\Data\\mnist\\27.03_logs\\{:}_mnist_100_9_8.txt'.format(type)
-
-    if taskName == 'cifar' and type == 'KS':
-        return '..\\Data\\cifar\\02.04 logs\\{:}_cifar_1000_9_8_total.txt'.format(type)
+        return '..\\Data\\mnist\\27.03_logs\\{:}_mnist_{:}_9_8.txt'.format(type, amount)
 
     if taskName == 'cifar':
-        return '..\\Data\\cifar\\02.04 logs\\{:}_cifar_100_9_8_total.txt'.format(type)
+        return '..\\Data\\cifar\\02.04 logs\\{:}_cifar_{:}_9_8_total.txt'.format(type, amount)
 
     return '..\\Data\\'
 
