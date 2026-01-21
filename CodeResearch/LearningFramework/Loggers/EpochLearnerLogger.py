08609fcc01525eec3305f26683f878073532f5b0
nrmanutd <nrmanutd@gmail.com>
2026-01-22 00:29:51 +0300

    cifar estimation started top fraction subsample

--- CodeResearch/LearningFramework/Loggers/EpochLearnerLogger.py
+++ CodeResearch/LearningFramework/Loggers/EpochLearnerLogger.py
@@ -12,7 +12,7 @@ class EpochLearnerLogger(BaseLogger):
         self.nArrays = nArrays
         self.nRepeats = nRepeats
         self.nAttempts = nAttempts
-        self.prefix = f'{prefix}_chain_main_standard_top_fraction_sampeled'
+        self.prefix = f'{prefix}_chain_main_standard_top_fraction_sampeled_2'
         self.taskName = taskName
         self.epochs = epochs
         self.counter = 0
