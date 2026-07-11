77e63d21b2f634c7f4efd247cd88fcc669b79f1c
nrmanutd <nrmanutd@gmail.com>
2026-07-11 15:53:46 +0300

    experiment for cifar100 and cifar10

--- CodeResearch/CurriculumLearning/generatePriorities.py
+++ CodeResearch/CurriculumLearning/generatePriorities.py
@@ -24,7 +24,7 @@ trainAlpha = 1
 testAlpha = 0.5
 
 #taskNames = ['mnist_epoch', 'cifar_epoch', 'cifar100_epoch']
-taskNames = ['cifar_epoch']
+taskNames = ['cifar100_epoch']
 logger = SimpleLogger()
 
 for i in range(0, len(taskNames)):
@@ -38,7 +38,7 @@ for i in range(0, len(taskNames)):
     methods_addedHardness = ['h&GradNorm_inc', 'h&EL2N_inc', 'h&cos_inc', 'h&entropy_inc', 'h&k-centered_inc']
 
     #restMethods = ['forgetting', 'k-centered_inc', 'h&k-centered_inc', 'cos_to_train_inc']
-    restMethods = ['h&cosToTrain_inc', 'chg_inc']
+    restMethods = ['h&cosToTrain_inc', 'cosToTrain_inc', 'chg_inc']
 
     methods_to_iterate = restMethods
     trainedModelsList = []
