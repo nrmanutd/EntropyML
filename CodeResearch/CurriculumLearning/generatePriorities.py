fixed h cos to train calculation

CodeResearch/CurriculumLearning/generatePriorities.py
@@ -24,7 +24,7 @@ trainAlpha = 1
 testAlpha = 0.5
 
 #taskNames = ['mnist_epoch', 'cifar_epoch', 'cifar100_epoch']
-taskNames = ['cifar_epoch', 'cifar100_epoch']
+taskNames = ['cifar_epoch']
 logger = SimpleLogger()
 
 for i in range(0, len(taskNames)):
@@ -34,11 +34,11 @@ for i in range(0, len(taskNames)):
     nClasses = len(np.unique(y))
 
     methods = ['rand', 'GradNorm', 'EL2N', 'cos', 'entropy', 'h', 'e', 'forgetting'] #todo: add rand
-    methods_inc = ['GradNorm_inc', 'EL2N_inc', 'cos_inc', 'cos_to_train_inc', 'entropy_inc', 'h_inc', 'e_inc', 'k-centered_inc', 'chg_inc']
+    methods_inc = ['GradNorm_inc', 'EL2N_inc', 'cos_inc', 'cosToTrain_inc', 'entropy_inc', 'h_inc', 'e_inc', 'k-centered_inc', 'chg_inc']
     methods_addedHardness = ['h&GradNorm_inc', 'h&EL2N_inc', 'h&cos_inc', 'h&entropy_inc', 'h&k-centered_inc']
 
     #restMethods = ['forgetting', 'k-centered_inc', 'h&k-centered_inc', 'cos_to_train_inc']
-    restMethods = ['chg_inc']
+    restMethods = ['h&cosToTrain_inc', 'chg_inc']
 
     methods_to_iterate = restMethods
     trainedModelsList = []